f1c0e3b4fe1d217b9610faf3f9fa65807364a888
EC2 Default User <ec2-user@ip-172-31-0-248.ec2.internal>
2022-01-10 11:19:41 +0000

    [Finish] 2-6

---
+++ lesson6.rb
@@ -0,0 +1,22 @@
+total_price = 90
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts  "みかんを購入。所持金は0円"
+else
+  puts "みかんを購入することはできません。"
+end
+
+# if 条件1
+  # 処理1  # 条件1がtrueだったら処理1を行う
+# elsif 条件2
+  # 処理2  # 条件1がfalse、かつ条件2がtrueだったら処理2を行う
+# elsif 条件3
+  # 処理3  # 条件1と条件2がfalse、かつ条件3がtrueだったら処理3を行う
+# ...(中略)...
+# elsif 条件n
+  # 処理n  # 条件1～条件n-1が全てfalse、かつ条件nがtrueだったら処理nを行う
+# else
+  # 処理x  # 条件1～条件nが全てfalseだったら処理xを行う
+# end
